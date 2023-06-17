7e46d26bbe34c79d8dcaff0105e1049ef95a7be0
qualwebs <adam@qualwebs.com>
2023-06-17 20:14:11 +0530

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
